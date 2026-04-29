fc6971eb8e4f388de3e45538f3bc96966b3d8b5f
arahim00026-art <a.rahim00026@gmail.com>
2026-04-29 15:14:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-# Stage the files
-!git add app.py
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-# Create a commit
-!git commit -m "Initial commit: Ibnsina Pharma Streamlit Dashboard"
-
-# Push to the main branch
-!git push -u origin main
